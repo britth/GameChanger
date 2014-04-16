df811def7dbea4f2ea4f664b066a5c9ef48c3277
britth <bmhayes@live.unc.edu>
2014-04-16 18:03:28 +0000

    Incorporating main menu, broke out separate functions within gamechanger function

--- gamechanger.py
+++ gamechanger.py
@@ -1,6 +1,6 @@
 import time
 import datetime
-
+import test
 
 ###Use to format timestamps to unix format
 def unix_time(timestamp):
@@ -25,15 +25,14 @@ def GameChanger(tournamentTweets):
 
   ###Data gathered using Twitter Streaming API
   ###Goal is eventually to stream the tweets directly in real time instead of reading in file
-
   tweets = open(tournamentTweets)
   tweets = tweets.readlines()
-  tweets = [tweet.lower() for tweet in tweets]
+  tweets = [tweet.lower() for tweet in tweets]  #make lower to avoid missing matches due to case differences
 
 
   ###Currently testing, so timestamp is simply first timestamp of input file
   ###Eventually, timestamp will be current time (since stream would be real-time)
-
+  ###For timestamps from dataset, have to subtract 14400 to convert to daylight savings EST (natively in GMT)
   timestamp = unix_time(tweets[0][:tweets[0].find(',')])-14400
   print timestamp
   searchingFor = {}
@@ -46,6 +45,7 @@ def GameChanger(tournamentTweets):
   ###Eventually read in from API or external files
   ###Data gathered from news article & ESPN Teams API
 
+  ###Key = matchup, Value = full matchup name
   matchups = {'WichitaStVKentucky': 'Wichita State v. University of Kentucky', 		
 		'IowaStVUNC': 'Iowa State v. University of North Carolina',			
 		'TennesseeVMercer': 'Tennessee v. Mercer',		
@@ -54,26 +54,26 @@ def GameChanger(tournamentTweets):
 		}
 
   ###Key = matchup, Value = start time
-
+  ###Times in EST
   schedule = {'WichitaStVKentucky': '03/23/2014 14:45:00',
 		'IowaStVUNC': '03/23/2014 17:15:00',
-		'TennesseeVMercer': '03/23/2014 18:10:00',   #ACTUAL TIME IS 18:10:00
+		'TennesseeVMercer': '03/23/2014 18:10:00',
 		'UCLAVStephenFAustin': '03/23/2014 19:10:00',
 		'CreightonVBaylor': '03/23/2014 19:45:00'
 		}
   
-  #times not exact, estimated based on dataset
-  #last three games didn't end in time period, so times not estimated
-  
-  end = {'WichitaStVKentucky': '03/23/2014 17:28:44',
-		'IowaStVUNC': '03/23/2014 19:38:00',
+  ###Key = matchup, Value = 10 minutes past estimated end time
+  ###Times not exact, estimated based on dataset (approximate end time + 10 minutes)
+  ###Last three games didn't end in time period, so times not estimated (just set equal to end of day)
+  ###Times in EST
+  end = {'WichitaStVKentucky': '03/23/2014 17:38:00',
+		'IowaStVUNC': '03/23/2014 19:48:00',
 		'TennesseeVMercer': '03/23/2014 23:59:00',
 		'UCLAVStephenFAustin': '03/23/2014 23:59:00',
 		'CreightonVBaylor': '03/23/2014 23:59:00'
 		}
 
   ###Key = matchup, Value = TV channel
-
   tv = {'WichitaStVKentucky': 'CBS',
 		'IowaStVUNC': 'CBS',
 		'TennesseeVMercer': 'TNT',
@@ -81,65 +81,25 @@ def GameChanger(tournamentTweets):
 		'CreightonVBaylor': 'truTV'
 		}
 
-  ###Key = matchup, Values = team abbreviations
-  ###Later, use abbreviations, name, and nickname as values
-
-  searchTerms = {'WichitaStVKentucky': ['WICH', 'UK', 'Shockers', 'Wichita St', 'Wildcats', 'Kentucky'], 		#, 'Shockers', 'Wichita State', 'Wildcats', 'Kentucky'
-		'IowaStVUNC': ['ISU', 'UNC', 'Cyclones', 'Iowa St', 'Tar Heels', 'North Carolina'],			#, 'Cyclones', 'Iowa State', 'Tar Heels', 'North Carolina'
-		'TennesseeVMercer': ['TENN', 'MER', 'Volunteers', 'Tennessee', 'Bears', 'Mercer'],			#, 'Volunteers', 'Tennessee', 'Bears', 'Mercer'
-		'UCLAVStephenFAustin': ['UCLA', 'SFA', 'Bruins', 'Lumberjacks', 'SF Austin'],				#, 'Bruins', 'Lumberjacks', 'SF Austin'
-		'CreightonVBaylor': ['CREI', 'BAY', 'Bluejays', 'Creighton', 'Bears', 'Baylor']				#, 'Bluejays', 'Creighton', 'Bears', 'Baylor'
+  ###Key = matchup, Values = team abbreviations, name, nickname
+  searchTerms = {'WichitaStVKentucky': ['WICH', 'UK', 'Shockers', 'Wichita St', 'Wildcats', 'Kentucky'],
+		'IowaStVUNC': ['ISU', 'UNC', 'Cyclones', 'Iowa St', 'Tar Heels', 'North Carolina'],
+		'TennesseeVMercer': ['TENN', 'MER', 'Volunteers', 'Tennessee', 'Bears', 'Mercer'],
+		'UCLAVStephenFAustin': ['UCLA', 'SFA', 'Bruins', 'Lumberjacks', 'SF Austin'],
+		'CreightonVBaylor': ['CREI', 'BAY', 'Bluejays', 'Creighton', 'Bears', 'Baylor']
 		}
 
 
   ###check time to ensure matchup is occurring (loop every 30 secs)
   
   while timestamp < unix_time(tweets[len(tweets)-1][:tweets[len(tweets)-1].find(',')])-14400:	#i.e., reached end of file #was +30
-    for key, value in schedule.iteritems():
-      if unix_time(value) <= timestamp:
-        searchingFor[key] = searchTerms[key]
-    #print searchingFor
-    for key, value in end.iteritems():
-      if key in searchingFor:
-        if unix_time(value) <= timestamp:
-          del searchingFor[key]
-    
-        
-
-    ###print sched if any matchups have changed
-    new_sched = get_sched(searchingFor, timestamp, matchups, schedule, tv)
-    if new_sched != current_sched:
-      current_sched = new_sched
-      print '\033[1m\n==========================================\nCURRENT GAMES (as of ' + str(est_time(timestamp))+ ')\n=========================================='
-      for key in searchingFor:
-        hoursAgo = timestamp - unix_time(schedule[key])
-        print matchups[key] + '\t==\tNetwork: '+ tv[key] + '\t==\tScheduled Start: ' + schedule[key] + ' (' + str(round(hoursAgo/3600, 2)) +' hours ago)'
-      print '==========================\n\033[0m'
-      #print current_sched
-    #print '\nTotal activity up to time: ' + str(est_time(timestamp))
-    
-
-    ###if matchup is occurring, check tweet text to see if it contains mention of matchup
-    ###sum these instances for each matchup
-    for tweet in tweets: 
-      if (unix_time(tweet[:tweet.find(',')])-14400 < timestamp):
-          continue
-      elif (unix_time(tweet[:tweet.find(',')])-14400 >= timestamp) and (unix_time(tweet[:tweet.find(',')])-14400 < (timestamp+30)):
-        for key, values in searchingFor.items():
-          for value in values: 
-            if value.lower() in tweet[tweet.find(',')+1:]:
-              tweetRates[key] = tweetRates.get(key, 0)+1
-              tweetsPerInterval[key] = tweetsPerInterval.get(key, 0)+1
-              break
-        tweets.remove(tweet) #NOT SURE? trying to make more efficient?
-        #print len(tweets)
-      else:
-        #print str(tweetsPerInterval) + ": THIS IS PER INTERVAL"
-        #tweetsPerInterval.clear()
-        break
+    searchingFor = search_terms(searchingFor, timestamp, searchTerms, schedule, end)
+    current_sched = print_sched(searchingFor, timestamp, matchups, schedule, tv, current_sched)
+    tweetsPerInterval, tweetRates, tweets = find_current_totals(tweets, tweetRates, searchingFor, timestamp)
+    #print len(tweets)
+       
+    #print tweetsPerInterval.values()
     timestamp = timestamp + 30
-    ##print tweetRates
-    ##print tweetsPerInterval
     greatest = 0
     for value in tweetsPerInterval.values():
       if value > greatest:
@@ -149,15 +109,63 @@ def GameChanger(tournamentTweets):
       greatestKey = tweetsPerInterval.keys()[tweetsPerInterval.values().index(greatest)]
       print "-----------------------------------------------------------\nThe most tweeted about game is: " + matchups[greatestKey] +" (" +str(greatest) + " tweets)\nWatch it live on "+ tv[greatestKey]+"!\n-----------------------------------------------------------"
     tweetsPerInterval.clear()
+  print tweetRates
+
+
+#find current search terms
+def search_terms(searchingFor, timestamp, searchTerms, schedule, end):
+  searchingFor = searchingFor
+  for key, value in schedule.iteritems():
+    if unix_time(value) <= timestamp:
+      searchingFor[key] = searchTerms[key]
+    #print searchingFor
+  for key, value in end.iteritems():
+    if key in searchingFor:
+      if unix_time(value) <= timestamp:
+        del searchingFor[key]
+  return searchingFor
+
+
+#print schedule if there has been a change in lineup (i.e. a new game has started or a game has ended)
+def print_sched(searchingFor, timestamp, matchups, schedule, tv, current_sched):
+  new_sched = get_sched(searchingFor, timestamp, matchups, schedule, tv)
+  if new_sched != current_sched:
+    current_sched = new_sched
+    print '\033[1m\n==========================================\nCURRENT GAMES (as of ' + str(est_time(timestamp))+ ')\n=========================================='
+    for key in searchingFor:
+      hoursAgo = timestamp - unix_time(schedule[key])
+      print matchups[key] + '\t==\tNetwork: '+ tv[key] + '\t==\tScheduled Start: ' + schedule[key] + ' (' + str(round(hoursAgo/3600, 2)) +' hours ago)'
+    print '==========================\n\033[0m'
+  return new_sched
+    #print current_sched
+  #print '\nTotal activity up to time: ' + str(est_time(timestamp))
+
+
+#calculate totals based on presence of one of the search strings
+#if any of the matchup strings are found, that key is incremented
+#if a tweet mentions more than one matchup, both keys are incremented
+def find_current_totals(tweets, tweetRates, searchingFor, timestamp):
+  tweetsPerInterval = {}
+  tweetRates = tweetRates
+  for tweet in tweets: 
+    if (unix_time(tweet[:tweet.find(',')])-14400 < timestamp):
+        continue
+    elif (unix_time(tweet[:tweet.find(',')])-14400 >= timestamp) and (unix_time(tweet[:tweet.find(',')])-14400 < (timestamp+30)):
+      for key, values in searchingFor.items():
+        for value in values: 
+          if value.lower() in tweet[tweet.find(',')+1:]:
+            tweetRates[key] = tweetRates.get(key, 0)+1 #MIGHT EDIT OUT
+            tweetsPerInterval[key] = tweetsPerInterval.get(key, 0)+1
+            break
+      tweets.remove(tweet) #NOT SURE? trying to make more efficient?
+    else:
+      break
+  return (tweetsPerInterval, tweetRates, tweets)
 
 
 
-  ###find volume, peak activity
+#Input is a data set of tweets containing 'marchmadness' or 'ncaa' from evening of March 23
+#timestamps are in GMT
+#GameChanger('NCAA23Mar2014Tweets.txt')
+GameChanger(test.main_menu())
 
-
-  ###if a game ends, print remaining games occurring at present
-
-
-
-#Currently, just using a hand created basic text file to test, not the real data set
-GameChanger('data/NCAA23Mar2014Tweets.txt')
